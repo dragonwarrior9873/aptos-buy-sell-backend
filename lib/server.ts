87ccec48702e375284d55fd5fd5166c1833524bc
dragonwarrior9873 <dragonwarrior9873@gmail.com>
2024-06-28 16:23:26 +0300

    backend completed

--- lib/server.ts
+++ lib/server.ts
@@ -4,22 +4,21 @@ import * as fs from 'fs';
 import * as cors from 'cors';
 
 const PORT = 3100;
-
-// Define CORS options
-const corsOptions: cors.CorsOptions = {
-    origin: 'http://localhost:3000', // Allow only this origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow only these methods
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-    optionsSuccessStatus: 204 // Some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-
-// Enable CORS with the specified options
-app.use(cors(corsOptions));
-
 const httpsOptions = {
     key: fs.readFileSync('./config/key.pem'),
     cert: fs.readFileSync('./config/cert.pem')
 };
+
+
+// Configure CORS options
+const allowedOrigins = [''];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+// Use CORS middleware with options
+app.use(cors(options));
 
 https.createServer(httpsOptions, app).listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
